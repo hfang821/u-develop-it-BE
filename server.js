aec628f5d7210f8c22b4aabde7b7060418a580a0
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-05 16:59:21 -0400

    adding the mysql query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 //import express
 const express = require('express');
 //adding port designations
@@ -8,13 +9,32 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username
+        user: 'root',
+        //MySQL password
+        password:'fanghaoyu',
+        database:'election'
+    },
+    console.log('Connected to the election database')
+);
+
 app.get('/',(req,res) => {
     res.json({
         message: 'Hello World!'
     });
 });
 
-//Default response for any other request(Not found)
+//the query() method runs the sql query and executes the callback with
+//all the resulting rows that match the query
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+});
+
+//catchAll: Default response for any other request(Not found)
 //Need to place this at the last one to handle other unavailable requests
 app.use((req, res) => {
     res.status(404).end();
